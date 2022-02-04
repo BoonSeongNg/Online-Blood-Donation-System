3703ee4d058b0fac118e857a54b6b4d3b8229378
Billy <Billy@Billy>
2022-02-04 14:53:12 +0800

    small changes

--- src/online/blood/donation/system/OnlineBloodDonationSystem.java
+++ src/online/blood/donation/system/OnlineBloodDonationSystem.java
@@ -15,7 +15,7 @@ public class OnlineBloodDonationSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("he");
+        System.out.println("hello");
     }
     
 }
